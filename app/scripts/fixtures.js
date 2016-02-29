1fc6a33bac0b06838f0886a3221816fc9328be96
Nicole Wade <Nicole.Wade@target.com>
2016-02-28 18:28:36 -0600

    c5-Added controllers for landing and collection pages

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,50 @@
+var albumPicasso = {
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+         { name: 'Blue', length: 161.71, audioUrl: '/assets/music/blue' },
+         { name: 'Green', length: 103.96, audioUrl: '/assets/music/green' },
+         { name: 'Red', length: 268.45, audioUrl: '/assets/music/red' },
+         { name: 'Pink', length: 153.14, audioUrl: '/assets/music/pink' },
+         { name: 'Magenta', length: 374.22, audioUrl: '/assets/music/magenta' }
+    ]
+};
+
+var albumMarconi = {
+     name: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { name: 'Hello, Operator?', length: '1:01' },
+         { name: 'Ring, ring, ring', length: '5:01' },
+         { name: 'Fits in your pocket', length: '3:21'},
+         { name: 'Can you hear me now?', length: '3:14' },
+         { name: 'Wrong phone number', length: '2:15'}
+     ]
+ };
+
+ var albumWest = {
+     name: '808s & Heartbreak',
+     artist: 'Kanye West',
+     label: 'EM',
+     year: '2008',
+     albumArtUrl: 'assets/images/album_covers/808s_&_Heartbreak.png',
+     songs: [
+         { name: 'Say You Will', length: '6:17' },
+         { name: 'Welcome to Heartbreak', length: '4:22' },
+         { name: 'Heartless', length: '3:30'},
+         { name: 'Amazing', length: '3:58' },
+         { name: 'Love Lockdown', length: '4:30'},
+         { name: 'Paranoid', length: '4:37'},
+         { name: 'RoboCop', length: '4:34'},
+         { name: 'Street Lights', length: '3:09'},
+         { name: 'Bad News', length: '3:58'},
+         { name: 'See You in My Nightmares', length: '4:18'},
+         { name: 'Coldest Winter', length: '2:44'}
+     ]
+ };
