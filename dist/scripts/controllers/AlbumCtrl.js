dc1d26e1f34a5a666ed7849107973384e0f5209c
Nicole Wade <Nicole.Wade@target.com>
2016-03-06 19:04:27 -0600

    c6-Added service to retrieve album data

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,21 +1,24 @@
 (function(){
-    function AlbumCtrl() {
-        this.albumData = [
-             { name: 'Blue', length: 161.71, audioUrl: '/assets/music/blue' },
-             { name: 'Green', length: 103.96, audioUrl: '/assets/music/green' },
-             { name: 'Red', length: 268.45, audioUrl: '/assets/music/red' },
-             { name: 'Pink', length: 153.14, audioUrl: '/assets/music/pink' },
-             { name: 'Magenta', length: 374.22, audioUrl: '/assets/music/magenta' }
-            ];
-            this.albumInfo = [
-            { name: 'The Colors', 
-            artist: 'Pablo Picasso',
-            label: 'Cubism',
-            year: '1881',
-            albumArtUrl: '/assets/images/album_covers/01.png'}
-        ];
+    function AlbumCtrl(Fixtures) {
+        
+        this.albumData = Fixtures.getAlbum();
+//        this.albumData = [
+//             { name: 'Blue', length: 161.71, audioUrl: '/assets/music/blue' },
+//             { name: 'Green', length: 103.96, audioUrl: '/assets/music/green' },
+//             { name: 'Red', length: 268.45, audioUrl: '/assets/music/red' },
+//             { name: 'Pink', length: 153.14, audioUrl: '/assets/music/pink' },
+//             { name: 'Magenta', length: 374.22, audioUrl: '/assets/music/magenta' }
+//            ];
+//            this.albumInfo = [
+//            { name: 'The Colors', 
+//            artist: 'Pablo Picasso',
+//            label: 'Cubism',
+//            year: '1881',
+//            albumArtUrl: '/assets/images/album_covers/01.png'}
+//        ];
+        this.albumInfo = Fixtures.getAlbumInfo();
     }
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
