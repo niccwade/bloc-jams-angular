31e28796806cba88ca34186e4fee226e9dc35c93
Nicole Wade <Nicole.Wade@target.com>
2016-03-12 20:43:27 -0600

    a7-Added private playSong method and documentation in SongPlayer

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
     function SongPlayer(){
         var SongPlayer = {};
         
+        /**
+        * @desc current song playing through player
+        * @type {Object}
+        **/
         var currentSong = null;
         /**
         * @desc Buzz object audio file
@@ -10,7 +14,17 @@
         var currentBuzzObject = null;
         
         /**
-        * @function setSong
+        * @function playSong--private method
+        * @desc plays song in currentBuzzObject and sets song.playing to true
+        * @param {Object} song
+        **/
+        var playSong = function(song){
+           currentBuzzObject.play();
+            song.playing = true;
+        };
+        
+        /**
+        * @function setSong--private method
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
         * @param {Object} song
         **/
@@ -27,19 +41,29 @@
             currentSong = song;
         };
         
+        /**
+        * @function SongPlayer.play--public method on SongPlayer
+        * @desc sets and plays song if song clicked is not playing
+        * if current song playing is clicked and is paused, play current song
+        * @param {Object} song
+        **/
         SongPlayer.play = function(song){
             if(currentSong !== song){
                 currentSong = song;
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
-                } else if(currentSong ===song){
+                playSong(song);
+                } else if(currentSong === song){
                     if(currentBuzzObject.isPaused()){
-                        currentBuzzObject.play();
+                            playSong(song);
                     }
                 }                 
         };
         
+        /**
+        * @function SongPlayer.pause--public method on SongPlayer
+        * @desc pauses currently playing song and sets currently playing song to false
+        * @param {Object} song
+        **/
         SongPlayer.pause = function(song){
            currentBuzzObject.pause();
             song.playing = false;
